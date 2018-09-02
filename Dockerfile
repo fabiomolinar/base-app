c921c4b65e8fcf9d04019beb28e08617df3c4c0d
fabiomolinar <fabiomolinar@gmail.com>
2018-09-02 20:57:01 +0200

    changing node port to something different than port 80

--- Dockerfile
+++ Dockerfile
@@ -15,7 +15,7 @@ RUN npm install
 
 COPY . .
 
-EXPOSE 80
+EXPOSE 3000
 CMD [ "npm", "start" ]
 
 USER node
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require("http");
 http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    response.end('Hello World\n');
-}).listen(80);
+}).listen(3000);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:80/');
+console.log('Server running at http://127.0.0.1:3000/');
